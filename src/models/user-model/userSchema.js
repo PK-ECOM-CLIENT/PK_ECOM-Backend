0fccd95bbf228eb205b6773f9611f7d3feea6bce
PK <pradeepdhital124@gmail.com>
2023-07-20 23:50:22 +1000

    working on use registration, developing schema

---
+++ src/models/user-model/userSchema.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    maxLength: 50,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    maxLength: 50,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    maxLength: 50,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    maxLength: 50,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    maxLength: 50,
+    required: true,
+  },
+  Address: {
+    street: {
+      type: String,
+      required: true,
+    },
+    street: {
+      type: String,
+      required: true,
+    },
+    street: {
+      type: String,
+      required: true,
+    },
+    street: {
+      type: String,
+      required: true,
+    },
+  },
+});
