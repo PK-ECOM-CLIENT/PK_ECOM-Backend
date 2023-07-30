38e2fe767b51ed22e5fc1a2c7f10e6ba87954766
PK <pradeepdhital124@gmail.com>
2023-07-30 16:52:53 +1000

    authentication and authorization done

--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -7,7 +7,25 @@ import {
 import { comparePassword, hashPassword } from "../helpers/bcryptHelper.js";
 import { v4 as uuidv4 } from "uuid";
 import { verificationEmail } from "../helpers/emailHelper.js";
+import { createJWTs, signAccessJWT, verifyRefreshJWT } from "../helpers/jwtHelper.js";
+import { userAuth } from "../middlewares/authMiddleware.js";
 const router = express.Router();
+// get user by access token, helping to auto login
+router.get("/", userAuth, async (req, res, next) => {
+  try {
+    const user = req.userInfo;
+    user.password = undefined;
+    user.refreshJWT = undefined;
+    res.json({
+      status: "success",
+      message: "user has been returned",
+      user,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.post("/", async (req, res, next) => {
   try {
     const { password } = req.body;
@@ -60,10 +78,13 @@ router.post("/login", async (req, res, next) => {
       const isMatched = comparePassword(password, user.password);
       if (isMatched) {
         user.password = undefined;
+        // jwt
+        const jwts = await createJWTs({ email });
         return res.json({
           status: "success",
           message: "logged in successfully",
           user,
+          ...jwts,
         });
       } else
         return res.json({
@@ -101,6 +122,31 @@ router.patch("/verify-email", async (req, res, next) => {
           message: "invalid or expired link, no action was taken",
         });
   } catch (error) {
+    next(error);
+  }
+});
+// generate new accessJWT and send back to the client
+router.get("/accessjwt", async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (authorization) {
+      // verify token
+      const decoded = verifyRefreshJWT(authorization);
+      if (decoded.email) {
+        // check if exist on db
+        const user = await findOneUser({ email: decoded.email });
+        if (user?._id) {
+          // create new accessJWT and send  to frontend
+          return res.json({
+            status: "success",
+            accessJWT: await signAccessJWT({ email: decoded.email }),
+          });
+        }
+      }
+    }
+    res.status(401).json({ status: "error", message: "unauthenticated" });
+  } catch (error) {
+    error.status = 401;
     next(error);
   }
 });
